3329be2d75bdc80ca8fa164b0c15c96520ea850b
sam <abluecar@gmail.com>
2023-11-10 15:55:12 -0500

    refactor: Abstract facebook to provider_callback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,23 +8,34 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
 
-  def facebook
+  def provider_callback(provider_name)
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider_name.capitalize) if is_navigational_format?
     else
-      session['devise.facebook_data'] = request.env['omniauth.auth']
+      session["devise.#{provider_name}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
 
-  def google_oauth2
+  def facebook
+    provider_callback('facebook')
+    # @user = User.from_omniauth(request.env['omniauth.auth'])
+    #
+    # if @user.persisted?
+    #   sign_in_and_redirect @user, event: :authentication
+    #   set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    # else
+    #   session['devise.facebook_data'] = request.env['omniauth.auth']
+    #   redirect_to new_user_registration_url
+    # end
   end
 
-  def github
-  end
+  def google_oauth2; end
+
+  def github; end
 
   # More info at:
   # https://github.com/heartcombo/devise#omniauth
