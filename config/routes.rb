fc5d1807abbf4c0cfcec7b3216e377cf0ef9f658
sam <abluecar@gmail.com>
2023-11-10 15:47:20 -0500

    feat: Update config files, credentials for OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@
 # root 'posts#index'
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   namespace :api do
     namespace :v1 do
       resources :posts, only: %i[index show create]
     end
   end
-  devise_for :users
 end
