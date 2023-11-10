6236828d1a7bfe4be63e995a10ed3003d3971a8b
sam <abluecar@gmail.com>
2023-11-10 15:42:52 -0500

    feat: Generate users/controllers and Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  pets_name              :string
+#  provider               :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  uid                    :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -21,7 +23,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook]
 
   has_many :requests_sent, class_name: 'FriendRequest', foreign_key: :sender_id, dependent: :destroy
   has_many :requests_received, class_name: 'FriendRequest', foreign_key: :recipient_id, dependent: :destroy
@@ -39,5 +42,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy, foreign_key: 'author_id'
 
   has_one :profile, dependent: :destroy
-  validates :pets_name, presence: true
+  # validates :pets_name, presence: true # TODO: Suggest that user add a pet, but not required
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
