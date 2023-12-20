fee7af665cda4a351e1c5f1b4a00c91406820334
sam <abluecar@gmail.com>
2023-12-20 02:46:25 -0500

    style: Show comments on a post, author info, etc.
    chore: Update seeds.rb
    style: Add icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,17 @@
 # creating a rake task
 # https://www.reddit.com/r/rails/comments/143j24q/seeding_the_db_best_approach/
 
-1.times do |n|
-  User.create!(email: "test_user@example.com",
-               username: "test_user",
+3.times do |n|
+  n += 1
+  User.create!(email: "test_user#{n}@example.com",
+               username: "test_user#{n}",
                password: 'password123')
 end
 
 4.times do |n|
+  n += 1
   User.create!(email: Faker::Internet.email,
-               username: "test_user#{n}",
+               username: "faker_user#{n}",
                password: 'password123')
 end
 
@@ -46,13 +48,18 @@ end
   random_past = random_time('past')
   random_user = User.find(rand(1..User.count))
   postable_types = [TextContent, PhotoContent]
-  Post.create!([
+  p = Post.create!([
                  { author: random_user,
                    postable: postable_types.sample.create!(content: 'My post.'),
                    created_at: random_past,
                    updated_at: random_past }
                ])
+
 end
+
+p = Post.create!(author: User.find(1), postable: TextContent.create!(content:'This is my post'))
+p.comments.create!(author_id:1, content:"My comment")
+p.comments.create!(author_id:2, content:"My comment2")
 
 # Nested comments
 # u = User.last
