f25fe5b632b20164b545c14a13302b061db16d4d
sam <abluecar@gmail.com>
2023-12-23 15:13:36 -0500

    feat: Create rough of share-button w/ pop-up

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,6 @@
 module CommentsHelper
+  # Shorten time ago, eg "11 minutes ago" -> "11m ago"
+  def abbreviate_time_ago
+    # <span><%= "#{time_ago_in_words(comment.created_at)} ago" %></span>
+  end
 end
