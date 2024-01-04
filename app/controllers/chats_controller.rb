3db1762cb9bcc17b67c10284653c8cbe378a5c17
sam <abluecar@gmail.com>
2024-01-03 19:06:11 -0500

    style: chats #index and #show

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,13 +4,14 @@ class ChatsController < ApplicationController
   def index
     @msg = Msg.new
     @msgs = Msg.includes(:user)
-
-    @users = User.all
+    @friends_usernames = current_user.all_friends_usernames
+    @friend_options = @friends_usernames.map { |v| [ v, v ] }
+    # @friend_options = User.all.map{ |u| [ u.name, u.id ] }
   end
 
   def show
     @sender = current_user
-    @recipient = User.find(params[:recipient_id])
+    @recipient = User.find_by(username: params[:chats][:recipient_username])
     @msgs = Msg.where(msg_sender_id: @sender.id, msg_recipient_id: @recipient.id)
                .or(Msg.where(msg_sender_id: @recipient.id, msg_recipient_id: @sender.id)) # Include inverse of pair
     @chat_id = [@sender.id, @recipient.id].sort.join('') # generates a unique identifier for a pair of user
--- app/javascript/channels/msg_channel.js
+++ app/javascript/channels/msg_channel.js
@@ -5,49 +5,24 @@ const chat_id = document.getElementById("chat_id").value;
 const msgChannel = consumer.subscriptions.create(
   { channel: "MsgChannel", chat_id: chat_id },
   {
-    // const msgChannel = consumer.subscriptions.create("MsgChannel", {
-    connected() {
-      // console.log(chat_id)
-    },
+    connected() {},
 
     disconnected() {},
 
     received(data) {
       const messageDisplay = document.querySelector("#message-display");
-      console.log(data);
+      document.getElementById("message-input").value = "";
       messageDisplay.insertAdjacentHTML("beforeend", this.template(data));
     },
     template(data) {
-      return `<div class="message">
-                <p>${data.msg_sender.username}: ${data.body}</p>
+      return `<div class="p-3 mb-4 bg-white rounded shadow w-fit">
+              <div class="mb-1 text-sm font-bold text-blue-400">
+                ${data.msg_sender.username}
+              </div>
+              <div>
+                ${data.body}
+              </div>
             </div>`;
     },
-
-    // template(data) {
-    //   return `<article class="message">
-    //             <div class="message-header">
-    //               <p>${data.user.email.split("@")[0]}</p>
-    //             </div>
-    //             <div class="message-body">
-    //               <p>${data.body}</p>
-    //             </div>
-    //           </article>`;
-    // },
   },
 );
-
-// TODO: Delete me
-// document.addEventListener("turbo:load", () => {
-//   let form = document.querySelector("#message-form");
-//   if (form) {
-//     form.addEventListener("submit", (e) => {
-//       e.preventDefault();
-//       let messageInput = document.querySelector("#message-input").value;
-//       if (messageInput == "") return;
-//       const inputData = {
-//         body: messageInput,
-//       };
-//       msgChannel.send({ inputData: inputData });
-//     });
-//   }
-// });
